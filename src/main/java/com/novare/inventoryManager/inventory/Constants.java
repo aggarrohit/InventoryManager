6d8e7ccd7642a3dd735d9adf52a002670371d596
aggarrohit <32900818+aggarrohit@users.noreply.github.com>
2023-07-19 14:00:10 +0200

    renamed assets folder to data

--- src/main/java/com/novare/inventoryManager/inventory/Constants.java
+++ src/main/java/com/novare/inventoryManager/inventory/Constants.java
@@ -3,5 +3,5 @@ package com.novare.inventoryManager.inventory;
 public class Constants {
 
     private Constants(){}
-    public static final String PATH_TO_ASSETS = System.getProperty("user.dir") + "/assets";
+    public static final String PATH_TO_ASSETS = System.getProperty("user.dir") + "/data";
 }
