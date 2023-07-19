c8656cef7082b2778a3431b51f9ce6182d5af781
Olga Pinchuk <pinchukolya@gmail.com>
2023-07-19 13:43:22 +0200

    remove extra lines

--- src/main/java/com/novare/inventoryManager/Main.java
+++ src/main/java/com/novare/inventoryManager/Main.java
@@ -2,10 +2,8 @@ package com.novare.inventoryManager;
 
 import com.novare.inventoryManager.menus.homeMenu.HomeMenu;
 
-
 public class Main {
     public static void main(String[] args) {
         new HomeMenu();
-
     }
 }
